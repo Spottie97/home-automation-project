8afafabbda36728563bc430d641eb2b8b47a5166
Reinhardt Erasmus <reinhardterasmus@gmail.com>
2023-06-18 09:32:36 +0200

    Added Philips hue call to turn on/off.

---
+++ main.py
@@ -0,0 +1,16 @@
+from mylights import HueLight
+
+# Replace with your actual bridge IP and username
+bridge_ip_address = 'your bridge ip address'
+username = 'your username'
+
+# Create HueLight objects for each of your lights
+# Replace with your actual light IDs
+light1 = HueLight(bridge_ip_address, username, 'light id 1')
+light2 = HueLight(bridge_ip_address, username, 'light id 2')
+light3 = HueLight(bridge_ip_address, username, 'light id 3')
+
+# Control the lights
+light1.turn_on()
+light2.turn_off()
+light3.turn_on()
