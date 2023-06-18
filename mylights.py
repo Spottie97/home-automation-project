c3539cebd5356a58108be7c160cd43afaeefd40e
Reinhardt Erasmus <reinhardterasmus@gmail.com>
2023-06-18 09:52:10 +0200

    Updated mylights

--- mylights.py
+++ mylights.py
@@ -17,20 +17,3 @@ class HueLight:
 
     def turn_off(self):
         return self.set_state(False)
-
-# Replace with your actual bridge IP and username
-bridge_ip_address = 'your bridge ip address'
-username = 'your username'
-
-# Create HueLight objects for each of your lights
-# Replace with your actual light IDs
-light1 = HueLight(bridge_ip_address, username, 'light id 1')
-light2 = HueLight(bridge_ip_address, username, 'light id 2')
-light3 = HueLight(bridge_ip_address, username, 'light id 3')
-
-# Control the lights
-light1.turn_on()
-light2.turn_off()
-light3.turn_on()
-
-
