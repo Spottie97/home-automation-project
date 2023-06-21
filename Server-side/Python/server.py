7709028c8183702802dd7d77de5b41dc966db203
Reinhardt Erasmus <reinhardterasmus@gmail.com>
2023-06-21 23:19:13 +0200

    Server Config Files
    - Made it so that the Bridge IP, username and Light ID are read from a JSON file instead of hardcode

--- Server-side/Python/server.py
+++ Server-side/Python/server.py
@@ -1,12 +1,22 @@
+import json
 from flask import Flask, request
 from mylights import HueLight
 
 app = Flask(__name__)
 
-# Replace with your actual bridge IP, username, and light IDs
-light1 = HueLight('bridge ip', 'username', 'light id 1')
-light2 = HueLight('bridge ip', 'username', 'light id 2')
-light3 = HueLight('bridge ip', 'username', 'light id 3')
+# Load the configuration from a JSON file
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+# Extract the values from the configuration
+bridge_ip = config['bridge_ip']
+username = config['username']
+lights_ids = config['lights']
+
+# Create the HueLight objects
+light1 = HueLight(bridge_ip, username, lights_ids[0])
+light2 = HueLight(bridge_ip, username, lights_ids[1])
+light3 = HueLight(bridge_ip, username, lights_ids[2])
 
 @app.route('/lights', methods=['POST'])
 def control_lights():
@@ -23,9 +33,24 @@ def control_lights():
             light1.set_color(data['light1']['color']['hue'], data['light1']['color']['sat'])
 
     # Control light 2
-    # You can add similar blocks for light 2 and light 3
-    # ...
-
+    if 'light2' in data:
+        if 'on' in data['light2']:
+            if data['light2']['on']:
+                light2.turn_on()
+            else:
+                light2.turn_off()
+        if 'color' in data['light2']:
+            light2.set_color(data['light2']['color']['hue'], data['light2']['color']['sat'])
+            
+    # Control light 3
+    if 'light3' in data:
+        if 'on' in data['light3']:
+            if data['light3']['on']:
+                light3.turn_on()
+            else:
+                light3.turn_off()
+        if 'color' in data['light3']:
+            light3.set_color(data['light3']['color']['hue'], data['light3']['color']['sat'])
     return 'OK'
 
 if __name__ == '__main__':
