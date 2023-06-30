bc00b3a8801df80079f9ca08c04adcaffb344ea9
Reinhardt Erasmus <126648334+ReinE97@users.noreply.github.com>
2023-06-30 18:34:40 +0200

    Added Logic to Light controller

--- Server/app/controllers/light_controller.py
+++ Server/app/controllers/light_controller.py
@@ -1,26 +1,52 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, current_app
 from app.services.light_service import LightService
 from app.services.error_service import ErrorService, CustomError
 
 light_controller = Blueprint('light_controller', __name__)
 
-# If lights cannot be created, let the exception propagate and crash the app
-light1, light2, light3 = LightService.create_lights()  
+# Define lights as a global variable
+lights = None
+
+@light_controller.before_app_first_request
+def create_lights():
+    global lights
+    try:
+        bridge_ip = current_app.config.get('BRIDGE_IP')
+        username = current_app.config.get('USERNAME')
+        lights_ids = current_app.config.get('LIGHT_IDS')  
+
+        lights = LightService.create_lights(bridge_ip, username, lights_ids)
+    except Exception as e:
+        ErrorService.raise_error(2003, additional_info=str(e))
 
 @light_controller.route('/lights', methods=['POST'])
 def control_lights():
+    global lights
     try:
         data = request.get_json()
 
         if not data:
             ErrorService.raise_error(2001)
 
-        # Add more specific error checks here based on what data you expect
-        # For example, if you expect a 'light_id' field:
         if 'light_id' not in data:
             ErrorService.raise_error(2002)
 
-        # TODO: Add logic here
+        if 'state' not in data:
+            ErrorService.raise_error(2006)
+
+        light_id = data['light_id']
+        state = data['state']
+
+        if light_id not in [1, 2, 3]:
+            ErrorService.raise_error(2007)
+
+        if state == 'on':
+            lights[light_id - 1].turn_on()
+        elif state == 'off':
+            lights[light_id - 1].turn_off()
+        else:
+            ErrorService.raise_error(2008)
+
         return 'OK'
     except CustomError as e:
         return {'error': str(e), 'error_code': e.error_code}, 400
